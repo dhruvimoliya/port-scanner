afd2ae177695cef49398d1f27acc8314ebbe11c8
dhruvimoliya <dhruvi21moliya@gmail.com>
2025-12-26 20:12:56 +0530

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -1,34 +1,40 @@
-"""
-Simple Port Scanner
-Author: Mirage43
-"""
-
 import socket
 
 def scan_port(target, port):
-    """Check if a port is open on the target."""
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
+        sock.settimeout(0.4)
+
         result = sock.connect_ex((target, port))
+
+        if result == 0 : 
+            try:
+                sock.send(b'HEAD / HTTP/1.1\r\n\r\n')
+
+                banner = sock.recv(1024).decode().strip()
+                clean_banner = banner.split('\n')[0]
+                sock.close()
+                return clean_banner
+            except:
+                sock.close()
+                return "Open (No Banner)"
+            
         sock.close()
-        return result == 0
+        return False
     except:
         return False
-
+    
 def main():
-    target = input("Enter target IP (e.g., 127.0.0.1): ")
-    
-    # Common ports to scan
-    ports = [21, 22, 80, 443, 3306, 8080]
-    
-    print(f"\nScanning {target}...\n")
-    
-    for port in ports:
-        if scan_port(target, port):
-            print(f"[✓] Port {port} is OPEN")
-        else:
-            print(f"[✗] Port {port} is CLOSED")
+    target_input = input("Enter targetIP/Domain (e.g.,scanme.nmap,org): ")
+    target = socket.gethostbyname(target_input)
 
+    print(f"\nScanning ports 1-1024 on {target} ...")
+
+    for port in range(1,1025):
+        response = scan_port(target, port)
+
+        if response:
+            print(f"[+] Port {port} is OPEN | Service: {response}")
+        
 if __name__ == "__main__":
     main()
